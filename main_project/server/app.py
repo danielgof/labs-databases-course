37cd4001e1172b0da23dbe1d362d214621ff406c
Daniil Gofman <gofmandon@gmail.com>
2022-12-10 13:15:03 +0300

    done with api

--- main_project/server/app.py
+++ main_project/server/app.py
@@ -23,8 +23,9 @@ def num_visits():
     count = get_hit_counts()
     app.logger.info(f"Number of visitors of portal at {datetime.today().strftime('%Y-%m-%d')} is {count}")
     return jsonify({"visited": count})
-
-
+"""
+get api
+"""
 @app.route("/api/v1/get_all_people_data", methods=["GET"])
 def get_all_data():    
     try:
@@ -56,6 +57,25 @@ def get_all_data():
         abort(Response(e, 500))
 
 
+@app.route("/api/v1/get_all_positions", methods=["GET"])
+def positions_info():
+    try:
+        data = session.query(Positon).all()
+        res = list()
+        for position in data:
+            res.append({
+            "position": position.position,
+            "salary": position.salary,
+            "department": position.departament
+            })
+        app.logger.info("All users were selected from database")
+        return res 
+    except Exception as e:
+        app.logger.warning(f"Exeption {e} ocured")
+        abort(Response(e, 500))
+"""
+add api
+"""
 @app.route("/api/v1/add_person", methods=["POST"])
 def add_person():
     data = request.get_json(force=True)
@@ -75,6 +95,20 @@ def add_person():
     return jsonify({"result": "succes"})
 
 
+@app.route("/api/v1/add_position", methods=["POST"])
+def add_position():
+    data = request.get_json(force=True)
+    position = Positon(
+        data["departament"],
+        data["salary"],
+        data["position"]
+    )
+    session.add(position)
+    session.commit()
+    return jsonify({"result": "success"})
+"""
+delete api
+"""
 @app.route("/api/v1/delete_person", methods=["DELETE"])
 def delete_person():
     data = request.get_json(force=True)
@@ -83,13 +117,29 @@ def delete_person():
     return jsonify({"result":"success"})
 
 
+@app.route("/api/v1/delete_phone", methods=["DELETE"])
+def delete_phone():
+    data = request.get_json(force=True)
+    session.query(Phone).filter(Person.phone == data["phone"]).delete()
+    session.commit()
+    return jsonify({"result":"success"})
+
+
+@app.route("/api/v1/delete_position", methods=["DELETE"])
+def delete_position():
+    data = request.get_json(force=True)
+    session.query(Positon).filter(Positon.id == data["id"]).delete()
+    session.commit()
+    return jsonify({"result": "success"})
+"""
+upd api
+"""
 @app.route("/api/v1/upd_phone", methods=["PUT"])
 def upd_phone():
     data = request.get_json(force=True)
     phone = session.query(Phone)\
     .filter(Phone.phone == data["phonenumber_old"])\
     .update({Phone.phone: data["phonenumber_new"]}, synchronize_session = False)
-    print(phone)
     session.commit()
     return jsonify({"result":"success"})
 
@@ -114,8 +164,8 @@ def upd_lastname():
     return jsonify({"result":"success"})
 
 
-@app.route("/api/v1/upd_position", methods=["PUT"])
-def upd_position():
+@app.route("/api/v1/upd_position_for_person", methods=["PUT"])
+def upd_position_for_person():
     data = request.get_json(force=True)
     session.query(Person)\
     .filter(Person.id == data["id"])\
@@ -124,37 +174,18 @@ def upd_position():
     return jsonify({"result":"success"})
 
 
-@app.route("/api/v1/add_position", methods=["POST"])
-def add_position():
+@app.route("/api/v1/upd_position", methods=["PUT"])
+def upd_position():
     data = request.get_json(force=True)
-    position = Positon(
-        data["departament"],
-        data["salary"],
-        data["position"]
-    )
-    session.add(position)
+    session.query(Positon)\
+    .filter(Positon.id == data["id"])\
+    .update({
+        Positon.departament: data["departament"], 
+        Positon.salary: data["salary"],
+        Positon.position: data["position"]
+        }, synchronize_session = False)
     session.commit()
-    return jsonify({"result": "success"})
-
-
-@app.route("/api/v1/positions_info", methods=["GET"])
-def positions_info():
-    try:
-        data = session.query(Positon).all()
-        res = list()
-        for position in data:
-            res.append({
-            "position": position.position,
-            "salary": position.salary,
-            "department": position.departament
-            })
-        app.logger.info("All users were selected from database")
-        return res 
-    except Exception as e:
-        app.logger.warning(f"Exeption {e} ocured")
-        abort(Response(e, 500))
-
+    return jsonify({"result":"success"})
 
 if __name__ == "__main__":
     app.run()
-
